3f0c966cafdaeaba6b49a0aa3675b9449e1304b0
AndreRH09 <aanazcoh@unsa.edu.pe>
2023-06-02 15:42:57 -0500

    Ejercicio2a y Ejercicio2b resueltos

---
+++ lab04_Python/pw2-lab-c-23a/lab04/EjerciciosPropuestos/Ejercicio2a.py
@@ -0,0 +1,10 @@
+from interpreter import draw
+from chessPictures import *
+
+"""ejercicio 1"""
+
+line1 = knight.join(knight.negative())
+line2 = knight.negative().join(knight)
+figure = line1.up(line2)
+
+draw(figure)
