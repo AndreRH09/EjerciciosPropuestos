c8abbb2027b3c513def295d4e8c025c487e3b6a6
AndreRH09 <aanazcoh@unsa.edu.pe>
2023-06-03 10:41:24 -0500

    ejer2b correjido

--- lab04_Python/pw2-lab-c-23a/lab04/EjerciciosPropuestos/Ejercicio2b.py
+++ lab04_Python/pw2-lab-c-23a/lab04/EjerciciosPropuestos/Ejercicio2b.py
@@ -4,7 +4,7 @@ from chessPictures import *
 """ejercicio 1"""
 
 line1 = knight.join(knight.negative())
-line2 = knight.negative().join(knight)
+line2 = knight.join(knight.negative())
 figure = line1.up(line2.horizontalMirror())
 
 draw(figure)
